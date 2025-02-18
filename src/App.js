d807d596eb6d13b907bd7ab7149e590b73dcb7d6
yakreb <berkayturna@gmail.com>
2025-02-18 21:13:03 +0100

    Add item input and quantity selection to the form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,13 +21,30 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
 
   function handleSubmit(event) {
     event.preventDefault();
+    if (!description) return;
+    const newItem = {
+      description,
+      quantity,
+      packed: false,
+      id: Date.now()
+    };
+    setDescription("");
+    setQuantity(1);
   }
-  return <form className="add-form" onSubmit={handleSubmit}>
+  return <form
+    className="add-form"
+    onSubmit={handleSubmit}
+  >
     <h3>What do you need for your 😍 trip?</h3>
-    <select>
+    <select
+      value={quantity}
+      onChange={(event) => setQuantity(event.target.value)}
+    >
       {Array.from({ length: 20 },
         (_, index) => index + 1)
         .map((number) =>
@@ -35,7 +54,14 @@ function Form() {
         )
       }
     </select>
-    <input type="text" placeholder="Item..." />
+    <input
+      type="text"
+      placeholder="Item..."
+      value={description}
+      onChange={
+        (event) => setDescription(event.target.value)
+      }
+    />
     <button>Add</button>
   </form>
 }
