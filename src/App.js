03700db6b65a97dbdacb6c7c1a4ba000fcd1561a
yakreb <berkayturna@gmail.com>
2025-02-19 12:13:54 +0100

    Enhance Stats component to display packing progress and handle empty state

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App() {
       <Logo />
       <Form handleAddItems={handleAddItems} />
       <PackingList items={items} handleDeleteItem={handleDeleteItem} handleToggleItem={handleToggleItem} />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -93,10 +93,25 @@ function Item({ item, handleDeleteItem, handleToggleItem }) {
   </li>;
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        Start adding some items to your packing list 🪄
+      </p>
+    );
+
+  const numOfItems = items.length
+  const numOfPackedItems = items.filter(item => item.packed === true).length
+  const percentage = Math.round(numOfPackedItems / numOfItems * 100)
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed Y (x%)</em>
+      <em>
+        {percentage === 100 ?
+          "You got everything! Ready to go ✈" :
+          `You have ${numOfItems} items on your list, and you already packed ${numOfPackedItems} (${percentage}%)`}
+      </em>
     </footer>
   )
 }
